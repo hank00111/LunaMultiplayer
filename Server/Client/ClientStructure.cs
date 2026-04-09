588569171574cc55cae7a2590631047c62824764
Drew Banyai <DrewBanyai@gmail.com>
2026-04-08 22:41:55 -0400

    Added LmpVersion to ClientStructure

--- Server/Client/ClientStructure.cs
+++ Server/Client/ClientStructure.cs
@@ -20,6 +20,7 @@ namespace Server.Client
 
         public string UniqueIdentifier { get; set; }
         public string KspVersion { get; set; }
+        public string LmpVersion { get; set; }
 
         public bool Authenticated { get; set; }
 
@@ -37,6 +38,8 @@ namespace Server.Client
         public ConcurrentQueue<IServerMessageBase> SendMessageQueue { get; } = new ConcurrentQueue<IServerMessageBase>();
         public int Subspace { get; set; } = int.MinValue; //Leave it as min value. When client connect we force them client side to go to latest subspace
         public float SubspaceRate { get; set; } = 1f;
+
+        public DateTime ConnectionTime { get; } = DateTime.UtcNow;
 
         public Task SendThread { get; }
 
