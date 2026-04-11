2d9e241bb458069e77db701aff64ab203d58c48f
Drew Banyai <DrewBanyai@gmail.com>
2026-04-11 01:50:19 -0400

    Thread-safety addition for RemovedVessels collection (O(n) -> O(1) as well!)

--- Server/System/Vessel/VesselActionGroupDataUpdater.cs
+++ Server/System/Vessel/VesselActionGroupDataUpdater.cs
@@ -18,7 +18,7 @@ namespace Server.System.Vessel
         public static void WriteActionGroupDataToFile(VesselBaseMsgData message)
         {
             if (!(message is VesselActionGroupMsgData msgData)) return;
-            if (VesselContext.RemovedVessels.Contains(msgData.VesselId)) return;
+            if (VesselContext.RemovedVessels.ContainsKey(msgData.VesselId)) return;
 
             //Sync part changes ALWAYS and ignore the rate they arrive
             _ = Task.Run(() =>
